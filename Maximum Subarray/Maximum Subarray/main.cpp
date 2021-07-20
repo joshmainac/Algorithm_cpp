63c5a6aa9b30414b6bb0a8362e6f16bb49826491
josh.mainac <josh.mainac@protonmail.com>
2021-07-20 09:44:49 -0700

    maxsub7-20-21

--- Maximum Subarray/Maximum Subarray/main.cpp
+++ Maximum Subarray/Maximum Subarray/main.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int maxSubArray(vector<int>& nums);
 
 void main()
 {
+	int x = 5;
+	int y = 6;
+
+	int z = max(5, 6);
+
+
+	vector<int> myvector;
+	myvector.push_back(1);
+	myvector.push_back(2);
+	myvector.push_back(3);
+	z= maxSubArray(myvector);
+	return;
 
 
 
@@ -18,8 +31,18 @@ void main()
 
 int maxSubArray(vector<int>& nums)
 {
+	int currentsum = 0;
+	int maxsum = 0;
+
+	for (int i = 0; i < nums.size(); i++)
+	{
+		currentsum += nums[i];
+		maxsum = max(currentsum, maxsum);
 
 
 
+	}
 
+
+	return maxsum;
 }
