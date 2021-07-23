1c333a3d8df8ee58b588e435fa543e96adb06549
josh.mainac <josh.mainac@protonmail.com>
2021-07-23 11:31:00 -0700

    staris7_23_21

--- Climbing Stairs/Climbing Stairs/main.cpp
+++ Climbing Stairs/Climbing Stairs/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
-
+int climbStairs_1(int n);
+int climbStairs_2(int n)
 
 
 int main()
 {
 
-
+    //int ans = climbStairs(45);
 
 
 
@@ -15,17 +16,16 @@ int main()
 }
 
 
-int climbStairs(int n) {
-    if (n <= 2) return n; // Since the solution for 0,1 and 2 is the number itself, we just return it without any further calculations
-    int temp1 = 1; // Imagine temp1 as dp[1] which stores number of ways to climb one step
-    int temp2 = 2; // Imagine temp2 as dp[2]
-    int ans = 0; // Ans is where we will store the value for n steps 
-    // Now to find the next number of steps, we keep in store the value of the last two and keep  adding it.
-    for (int i = 3; i < n + 1; i++)
-    {
-        ans = temp1 + temp2; // stores the value of n steps
-        temp1 = temp2; // pushes the value of temp1 and temp2 one step back for the next iteration
-        temp2 = ans;
-    }
+int climbStairs_1(int n) {
+    if (n ==0 || n == 1) return 1; 
+    int ans = climbStairs_1(n - 1) + climbStairs_1(n - 2);
     return ans;
+
+}
+
+int climbStairs_2(int n) {
+    if (n == 0 || n == 1) return 1;
+    int ans = climbStairs_2(n - 1) + climbStairs_2(n - 2);
+    return ans;
+
 }
