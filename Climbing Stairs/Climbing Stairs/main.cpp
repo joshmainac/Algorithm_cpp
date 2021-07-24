8dbfbb01e7f023d26f8201a014cc7d2c5679f2a5
josh.mainac <josh.mainac@protonmail.com>
2021-07-24 08:36:17 -0700

    stairs7-24-21

--- Climbing Stairs/Climbing Stairs/main.cpp
+++ Climbing Stairs/Climbing Stairs/main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 int climbStairs_1(int n);
-int climbStairs_2(int n)
+int climbStairs_2(int n);
 
 
 int main()
 {
 
-    //int ans = climbStairs(45);
+
+    int ans2 = climbStairs_2(1);
 
 
 
@@ -19,13 +20,27 @@ int main()
 int climbStairs_1(int n) {
     if (n ==0 || n == 1) return 1; 
     int ans = climbStairs_1(n - 1) + climbStairs_1(n - 2);
+    cout << ans << endl;
     return ans;
 
 }
 
 int climbStairs_2(int n) {
-    if (n == 0 || n == 1) return 1;
-    int ans = climbStairs_2(n - 1) + climbStairs_2(n - 2);
-    return ans;
+    int num1 = 1;
+    int num2 = 1;
+    int num3 = 1;
+
+    for (int i = 0; i< n-1; i++)
+    {
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+
+
+    }
+
+    cout << num3 << endl;
+    return num3;
+
 
 }
