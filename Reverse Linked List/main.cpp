53f0eae3e0013165f495e6fa52bf04fa2d4640b6
josh.mainac <josh.mainac@protonmail.com>
2021-08-05 09:42:14 -0700

    8-5-21

--- Reverse Linked List/main.cpp
+++ Reverse Linked List/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 using namespace std;
 
+ // Definition for singly-linked list.
+  struct ListNode {
+      int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+ 
+ ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+ 
 int main()
 {
-
+    ListNode a(1);
+    ListNode b(2);
+    ListNode c(3);
+    ListNode d(3);
+    ListNode e(3);
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &e;
 
 
 
