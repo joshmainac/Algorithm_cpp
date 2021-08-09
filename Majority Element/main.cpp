660a52ab05d2adcf50bde99d3b161e2e1bba4f74
josh.mainac <josh.mainac@protonmail.com>
2021-08-09 10:00:46 -0700

    complete 8-9-21

--- Majority Element/main.cpp
+++ Majority Element/main.cpp
@@ -1,13 +1,28 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 using namespace std;
 int majorityElement(vector<int>& nums);
+
+
+//Given an array nums of size n, return the majority element.
+//
+//The majority element is the element that appears more than ⌊n / 2⌋ times.You may assume that the majority element always exists in the array.
+//so if  length is 7 return must be a value that appears more than 7/2 = 3.5 time
 
 int main()
 {
 
 
-
+    vector<int> myvector;
+    myvector.push_back(1);
+    myvector.push_back(1);
+    myvector.push_back(1);
+    myvector.push_back(1);
+    myvector.push_back(2);
+    myvector.push_back(2);
+    myvector.push_back(3);
+    myvector.push_back(3);
+    int i = majorityElement(myvector);
 
 
 
@@ -16,9 +31,14 @@ int main()
 }
 
 int majorityElement(vector<int>& nums){
-	int leadnum_saved;
-	int leadnum_changing;
 
+    int element = 0, count = 0;
+    for (int n : nums)
+    {
+        if (count == 0) element = n;
+        n == element ? ++count : --count;
+    }
+    return element;
 
 
 
