7aabd156ce0c22c04dc9fbf91740b43b6a2271c1
josh.mainac <josh.mainac@protonmail.com>
2021-08-22 08:56:05 -0700

    8-22-21

--- Find All Numbers Disappeared in an Array/main.cpp
+++ Find All Numbers Disappeared in an Array/main.cpp
@@ -21,14 +21,23 @@ int main()
 
 vector<int> findDisappearedNumbers(vector<int>& nums)
 {
+	vector<int> ans;
 	for (int i = 0; i < nums.size(); i++)
 	{
-		if (nums[i] > 0)
+		if (nums[abs(nums[i])-1] > 0)
 		{
-			nums[i] *= -1;
+			nums[abs(nums[i]) - 1] *= -1;
 
 		}
 
+
+	}
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (nums[i] > 0 )
+		{
+			ans.push_back(i + 1);
+		}
 
 	}
 
@@ -37,7 +46,7 @@ vector<int> findDisappearedNumbers(vector<int>& nums)
 
 
 
-	return nums;
+	return ans;
 }
 
 
