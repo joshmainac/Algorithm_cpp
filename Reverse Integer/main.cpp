55e08ef0e978295202ace26c72c4eb07ec6cfd54
josh.mainac <joshua.long@wsu.edu>
2021-08-25 09:11:41 -0700

    8-25-21

--- Reverse Integer/main.cpp
+++ Reverse Integer/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <stack>
+#include <string> 
 using namespace std;
 int reverse(int x);
 //Input: x = 123
@@ -6,9 +8,7 @@ int reverse(int x);
 int main()
 {
 
-
-
-
+	cout << reverse(-123) << endl;
 
 
 
@@ -19,7 +19,38 @@ int main()
 
 
 int reverse(int x) {
+	//make x to string
+	string ans;
+	string aa;
+	if (x < 0)
+	{
+		ans = ans + "-";
+		aa = to_string(abs(x));
+	}
+	 
+	stack<int> mystack;
+	
+	
+	//push every charakter to stack
+	for (int i = 0; i < aa.size(); i++)
+	{
+		mystack.push(aa[i]-'0');
+	}
+	//pop every character and add it to string
+	while (!mystack.empty())
+	{
+		ans = ans + to_string(mystack.top());
+		mystack.pop();
+	}
+	// return string 
+	cout << ans << endl;
+	int gh= 9;
+	gh = gh + ans[1]-'0';
+	return 1;
+}
 
+int reverse_2(int x, int size) {
+	//make x to string
 
 
 	return 0;
